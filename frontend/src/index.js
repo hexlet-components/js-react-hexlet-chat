f62262740727f563bb989baeff6ab00246e2a88f
Ivan Gagarinov <dzencot@gmail.com>
2022-12-19 13:54:39 +0500

    fix init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App.jsx';
+import init from './init.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const app = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(await init());
+};
+
+app();
