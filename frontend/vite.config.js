b7ce2b41efdab664b9b2cea8dabc0abd1f915d3e
Elena Gromova <37748853+HelenOne@users.noreply.github.com>
2024-10-29 16:29:23 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5002,
     proxy: {
-      // Проксируем все запросы
-      '/': {
+      // Проксируем запросы к API
+      '/api': {
         target: 'http://localhost:5001',
-        changeOrigin: true,
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
       },
     },
   },
